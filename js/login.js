af5db608c8a023f77aa4bfafc0fbb473e3c92b4c
larry <15802127603@163.com>
2017-08-07 23:44:43 +0800

    撒大声地

--- js/login.js
+++ js/login.js
@@ -12,7 +12,35 @@ $(function(){
    	  window.location.href = 'dianzizhengshu.html';
    })
    
-   $(".submit").on("click",function(){
+   
+   //登录页链接
+   $(".submit1").on("click",function(){
    	 window.location.href = "Sys_Main.html";
+   })
+   $(".submit2").on("click",function(){
+   	 window.location.href = "login-repassword.html";
+   })
+   $(".go-login").on("click",function(){
+   	 window.location.href = "login.html";
+   })
+   $(".go-login-phone").on("click",function(){
+   	 window.location.href = "login-phone.html";
+   })
+   $(".go-login-company").on("click",function(){
+   	 window.location.href = "login-company.html";
+   })
+   //注册页面的复选框
+   $(".fuwuxieyi").on("click",function(){
+   	  if(!$(this).hasClass('active')){
+   	  	$(this).addClass('active');
+   	  }else{
+   	  	$(this).removeClass('active');
+   	  }
+   })
+   $(".fuwuxieyi-btn").on("click",function(){
+   	  $(".read-container").removeClass('hide');
+   })
+   $(".closed-btn").on("click",function(){
+   	  $(".read-container").addClass('hide');
    })
 })
